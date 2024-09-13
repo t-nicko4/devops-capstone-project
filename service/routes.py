a61771b6de371f7dae70506ed28948d147ff9fdf
Wilnick Jean <wilnick2009@hotmail.com>
2024-09-13 11:51:04 -0400

    Refactoring a test case

--- service/routes.py
+++ service/routes.py
@@ -82,10 +82,7 @@ def get_accounts(account_id):
         return account.serialize(), status.HTTP_200_OK
 
 
-def test_get_account_not_found(self):
-        """It should not Read an Account that is not found"""
-        resp = self.client.get(f"{BASE_URL}/0")
-        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
