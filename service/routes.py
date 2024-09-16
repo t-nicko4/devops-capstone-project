343aeaf088ec570c3eaf55f66c4fa49055a3111e
Wilnick Jean <45670681+t-nicko4@users.noreply.github.com>
2024-09-16 15:40:23 -0400

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -79,8 +79,7 @@ def get_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    return account.serialize(), status.HTTP_200_OK
-    
+    return account.serialize(), status.HTTP_200_OK    
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
